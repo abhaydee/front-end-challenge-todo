8ef9ce9cd9722c9abaee5ca68f214b59b88f6cf2
abhaydee <abhaygirish1997@gmail.com>
2021-01-07 16:11:40 +0530

    changes to format

--- src/redux/reducers/todoreducer.js
+++ src/redux/reducers/todoreducer.js
@@ -1,30 +1,34 @@
 import { act } from "react-dom/test-utils";
 import { ADD_TASK, DELETE_TASK, UPDATE_TASK } from "../actiontypes";
 
-export let intialState={
-    todos:[]
-}
+export let intialState = {
+  todos: [],
+};
 
-export const todoreducer=(state=intialState,action)=>{
-    switch(action.type){
-        case ADD_TASK:
-            return {
-                ...state,
-                todos:[...state.todos,action.payload]
-            }
-            break;
-        case DELETE_TASK:
-            return{
-                ...state,
-                todos:state.todos.splice(action.payload,1)
-            }
-        case UPDATE_TASK:
-            return {
-                ...state,
-                todos:state.todos.splice(action.payload.index,1,action.payload.edittask)
-            }
-        default:{
-            return state
-        }
+export const todoreducer = (state = intialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+      break;
+    case DELETE_TASK:
+      return {
+        ...state,
+        todos: state.todos.splice(action.payload, 1),
+      };
+    case UPDATE_TASK:
+      return {
+        ...state,
+        todos: state.todos.splice(
+          action.payload.index,
+          1,
+          action.payload.edittask
+        ),
+      };
+    default: {
+      return state;
     }
-}
+  }
+};
