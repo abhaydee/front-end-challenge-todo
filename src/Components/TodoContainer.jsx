46060aa041180ac562a2f8f0c9ae45949d60d067
abhaydee <abhaygirish1997@gmail.com>
2021-01-08 16:23:17 +0530

    changes to container

--- src/Components/TodoContainer.jsx
+++ src/Components/TodoContainer.jsx
@@ -1,30 +1,39 @@
-import React,{useState} from "react";
-import IconMoon from "../images/icon-moon.svg"
-import IconSun from "../images/icon-sun.svg"
+import React, { useState } from "react";
+import IconMoon from "../images/icon-moon.svg";
+import IconSun from "../images/icon-sun.svg";
 import styles from "../styles/TodoContainer.scss";
 import InputContainer from "./InputContainer";
-import {useSelector,useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
 import TodoListContainer from "./TodoListContainer";
 function TodoContainer() {
-    const [theme,setTheme]=useState(true)
-    const todolist=useSelector((state)=>state.todoreducer.todos)
-    console.log("the todo list",todolist)
-    const handleTheme=()=>{
-        setTheme(!theme)
-        useDispatch({type:"THEME",payload:theme})
-    }
+  const [theme, setTheme] = useState(true);
+  const todolist = useSelector((state) => state.todoreducer.todos);
+  console.log("the todo list", todolist);
+  const handleTheme = () => {
+    setTheme(!theme);
+    useDispatch({ type: "THEME", payload: theme });
+  };
   return (
-      <div className={`todo-container ${theme===true?"todo-container__light":"todo-container__dark"}`}>
+    <div>
+      <div
+        className={`todo-container ${
+          theme === true ? "todo-container__light" : "todo-container__dark"
+        }`}
+      >
         <div className={"todo-container__theme"}>
-            <h1 className={"todo-container__heading"}>T O D O</h1>
-            <img src={theme===true? IconSun:IconMoon} className={"todo-container__themeicon"} onClick={handleTheme} />
+          <h1 className={"todo-container__heading"}>T O D O</h1>
+          <img
+            src={theme === true ? IconSun : IconMoon}
+            className={"todo-container__themeicon"}
+            onClick={handleTheme}
+          />
         </div>
-        <InputContainer/>
+        <InputContainer />
         <div className="todolist-container">
-        <TodoListContainer/>
+          <TodoListContainer />
         </div>
       </div>
-
+    </div>
   );
 }
 
