2c0e34d766b09d1181cf5089e3046ff44306d563
abhaydee <abhaygirish1997@gmail.com>
2021-01-09 15:09:27 +0530

    updates to css

--- src/Components/TodoListContainer.jsx
+++ src/Components/TodoListContainer.jsx
@@ -5,16 +5,31 @@ import IconCheck from "../images/icon-check.svg";
 import IconCross from "../images/icon-cross.svg";
 function TodoListContainer() {
   const todolist = useSelector((state) => state.todoreducer.todos);
-  const theme=useSelector((state)=>state.todoreducer.theme)
-  console.log('the theme',theme)
+  const theme = useSelector((state) => state.todoreducer.theme);
+  console.log("the theme", theme);
   return (
     <>
       {todolist.map((item) => {
         return (
-          <div className="input-container" style={{backgroundColor:theme===true?"white":"#171723"}}>
-            <img src={IconCheck} alt="check-icon" className={"input-container__check"}/>
-            <p className="input-container__text">{item}</p>
-            <img src={IconCross} alt="delete-icon" className={"input-container__cross"}/>s
+          <div
+            className="input-container"
+            style={{ backgroundColor: theme === true ? "white" : "#171723" }}
+          >
+            <div className="input-container__child">
+              <div className="input-container__child__checkmark">
+                <img
+                  src={IconCheck}
+                  alt="check-icon"
+                  className={"input-container__check"}
+                />
+              </div>
+              <p className="input-container__text">{item}</p>
+              <img
+                src={IconCross}
+                alt="delete-icon"
+                className={"input-container__cross"}
+              />
+            </div>
           </div>
         );
       })}
