f63b06a159ace3ca3a46d411c69d55100cfa9682
abhaydee <abhaygirish1997@gmail.com>
2021-01-10 15:59:09 +0530

    refactoring conflicts in css

--- src/Components/TodoListContainer.jsx
+++ src/Components/TodoListContainer.jsx
@@ -5,7 +5,7 @@ import IconCheck from "../images/icon-check.svg";
 import IconCross from "../images/icon-cross.svg";
 import * as types from "../redux/actiontypes";
 function TodoListContainer() {
-  const [completed,setCompleted]=useState(false)
+  const [completed,setCompleted]=useState("")
   const todolist = useSelector((state) => state.todoreducer.todos);
   console.log("the todolist",todolist)
   const theme = useSelector((state) => state.todoreducer.theme);
@@ -26,24 +26,24 @@ function TodoListContainer() {
       {todolist.map((item,index) => {
         return (
           <div
-            className={`input-container input-container${theme===true?"__light":"__dark"}`}
+            className={`inputlist-container inputlist-container${theme===true?"__light":"__dark"}`}
             key={index}
           >
-            <div className="input-container__child">
-              <div className="input-container__child__checkmark" onClick={handleComplete}>
+            <div className="inputlist-container__child">
+              <div className="inputlist-container__child__checkmark" onClick={handleComplete}>
                 {completed===true &&
                   <img
                   src={IconCheck}
                   alt="check-icon"
-                  className={"input-container__check"}
+                  className={"inputlist-container__check"}
                 />
                 }
               </div>
-              <p className={`input-container__text input-container__text${completed===true?"__completed":""}`} onClick={()=>handleUpdate(index,item.input)}>{item.input}</p>
+              <p className={`inputlist-container__text inputlist-container__text${completed===true?"__completed":""}`} onClick={()=>handleUpdate(index,item.input)}>{item.input}</p>
               <img
                 src={IconCross}
                 alt="delete-icon"
-                className={"input-container__cross"}
+                className={"inputlist-container__cross"}
                 onClick={()=>handleDelete(index)}
               />
             </div>
